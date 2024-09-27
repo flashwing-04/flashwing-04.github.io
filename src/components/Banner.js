71f15fab778ee1943df6c3de2ae81c2bb9010cf5
flashwing-04 <charlotte.john.04@gmx.de>
2024-09-27 20:02:54 +0200

    revert

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ export const Banner = () => {
     // for typing Animation
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["undergrad Computer Science Student"]
+    const toRotate = ["undergrad Computer Science Student", "<placeholder for really cool text>"]
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300-Math.random()*100)
     const period = 2000;
@@ -52,7 +52,12 @@ export const Banner = () => {
                         <div id="headline">
                         <h1>{'Hi, I\'m an '}<span className="wrap">{text}</span></h1>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                        </p>
                         <button onClick={() => HTMLFormControlsCollection.log('connect')}>Let's connect </button>
                     </Col>
                     <Col xs={12} md={6} xl={5} className="col-small">
