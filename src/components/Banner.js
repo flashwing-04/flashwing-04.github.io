05de9f68f6763e1c775b313779a4bc5af4b90197
flashwing-04 <charlotte.john.04@gmx.de>
2024-12-22 15:09:01 +0100

    change styling banner slightly

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react"
-import { Container, Row, Col } from "react-bootstrap"
-
-import headerImg from "../assets/img/header_img.png"
 
 export const Banner = () => {
 
@@ -45,21 +42,12 @@ export const Banner = () => {
 
     return(
         <section className="banner" id="home">
-            <Container>
-                <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7} className="col-big">
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <div id="headline">
-                        <h1>{'Hi, I\'m an '}<span className="wrap">{text}</span></h1>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                        <button onClick={() => HTMLFormControlsCollection.log('connect')}>Let's connect </button>
-                    </Col>
-                    <Col xs={12} md={6} xl={5} className="col-small">
-                        <img src={headerImg} alt="Header Img" />
-                    </Col>
-                </Row>
-            </Container>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <div id="headline">
+                <h1>{'Hi, I\'m an '}<span className="wrap">{text}</span></h1>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <button onClick={() => HTMLFormControlsCollection.log('connect')}>Let's connect </button>
         </section>
     )
 }
